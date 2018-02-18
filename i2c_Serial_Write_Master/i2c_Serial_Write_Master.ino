bdc6c255bb84d5774eb67aa31f9d9544d12509ce
bleung329 <bleun@stuy.edu>
2018-02-18 16:54:57 -0500

    AAAAAAAAAAAAAAAAHH

---
+++ i2c_Serial_Write_Master/i2c_Serial_Write_Master.ino
@@ -0,0 +1,19 @@
+#include <Wire.h>
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("Master Initialized.");
+}
+
+void loop() {
+    Serial.println("Written");
+		Wire.beginTransmission(8); // transmit to device #8       
+		Wire.write("A");              // sends one byte
+		Wire.endTransmission();   
+    delay(500);
+    Wire.beginTransmission(8); // transmit to device #8       
+    Wire.write("B");              // sends one byte
+    Wire.endTransmission();  
+    delay(500);
+}
