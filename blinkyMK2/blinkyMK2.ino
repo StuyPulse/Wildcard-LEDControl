b455814e8ed7a0535a98f111e7aec82274ecd991
bleung329 <bleung@stuy.edu>
2018-02-26 12:03:05 -0500

    Someboby is gonna read these commit messages someday. Hello future person! I will switch to a Frames Per Second operating concept.

--- blinkyMK2/blinkyMK2.ino
+++ blinkyMK2/blinkyMK2.ino
@@ -1,11 +1,32 @@
 #include <Wire.h>
 #include <FastLED.h>
 
+#define ULPIN
+#define ULLEDCOUNT
+
+#define LLPIN 
+#define LLLEDCOUNT
+
+#define SELF_ADDRESS
+
+CRGB LLleds[LLLEDCOUNT];
+CRGB ULleds[ULLEDCOUNT];
+int patternID;
+
 void setup()
 {
-	
+  Wire.begin(SELF_ADDRESS);
+  Wire.onReceive(recvEvent);
+  Serial.begin(9600);
+  Serial.println(F("BMK2: Initialized :)"));
+  
 }
 void loop()
 {
-	
+  
+}
+
+void recvEvent(int numBytes)
+{
+
 }
