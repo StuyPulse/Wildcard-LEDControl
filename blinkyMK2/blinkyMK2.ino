6db2c14ab7e26f010327a4262014c11bc503871b
Brian Leung <brianleung329@gmail.com>
2018-03-15 13:55:47 -0400

    nvm pointers a bad idea abort abort

--- blinkyMK2/blinkyMK2.ino
+++ blinkyMK2/blinkyMK2.ino
@@ -1,46 +1,70 @@
+/*
+Made by DOE Brian Leung - Class of 2018
+
+Made for Teensy++ 2.0 and (will post board layout)
+
+For the future, if you guys ever want a pretty lit bot :(
+*/
+
 #include <Wire.h>
 #include <FastLED.h>
 
+#define FPS 60
+
 #define ULPIN 7
 #define ULLEDCOUNT 8
-/*
-#define LLPIN 
-#define LLLEDCOUNT
-
-#define SELF_ADDRESS
-*/
-//CRGB LLleds[LLLEDCOUNT];
 CRGB ULleds[ULLEDCOUNT];
+int ULpat
+int ULhold
+
+#define LLPIN 5
+#define LLLEDCOUNT 8
+CRGB LLleds[LLLEDCOUNT];
+int LLpat
+int LLhold
+
+#define SELF_ADDRESS 98
 
 void setup()
 {
   pinMode(6,OUTPUT);
-  //Wire.begin(SELF_ADDRESS);
+  Wire.begin(SELF_ADDRESS);
   Wire.onReceive(recvEvent);
   Serial.begin(9600);
   Serial.println(F("BMK2: Initialized :)"));
   FastLED.addLeds<WS2812,ULPIN,GRB>(ULleds,ULLEDCOUNT);
-  allSet(244,0,0,&ULleds);
+  FastLED.addLeds<WS2812,LLPIN,GRB>(LLleds,LLLEDCOUNT);
 }
+
 void loop()
 {
-  digitalWrite(6,HIGH);
-  delay(300);
-  digitalWrite(6,LOW);
-  delay(300);
+  updateFrame(ULpat,LLpat);
+  delay(1000/FPS);
 } 
 
 void recvEvent(int numBytes)
 {
-
+  if (Wire.available())
+  {
+    
+  }
 }
 
-void allSet(int R, int G, int B, CRGB *arr)
+void allSet(int R, int G, int B, int strip)
 {
-  int ledCount = sizeof(arr)/sizeof(arr[0]);
-  for (int index = 0; index<ledCount; index++)
+  if (strip)
   {
-    arr[index] = CRGB(R,G,B);
+    for (int index = 0; index<LLLEDCOUNT; index++)
+    {
+      LLleds[index] = CRGB(R,G,B);
+    }
+  }
+  else
+  {
+    for (int index = 0; index<ULLEDCOUNT; index++)
+    {
+      ULleds[index] = CRGB(R,G,B);
+    }
   }
   FastLED.show();
 }
